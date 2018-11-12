3d3f49345f3169dd6e7494129f25fd4c280ed2bb
romage <arome111@gmail.com>
2018-11-12 08:55:53 +0000

    simple get methods added to flashcard service

--- src/app/flashcards/flashcards.service.ts
+++ src/app/flashcards/flashcards.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
-
+import { Observable, pipe, of } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
+import { Card } from './models/card';
+import { HttpClient } from '@angular/common/http';
+import { nextContext } from '@angular/core/src/render3';
 @Injectable({
   providedIn: 'root'
 })
 export class FlashcardsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getCards(): Observable<Card[]> {
+    const url = '';
+    return this.http.get<Card[]>(url).pipe(
+      catchError(this.handleError('getCards', []))
+    );
+  }
+
+  getCardsForCategory(category: string): Observable<Card[]> {
+    const url = '';
+    return this.http.get<Card[]>(url).pipe(
+      catchError(this.handleError('getCardsForCategory', []))
+    );
+  }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+      // TODO: better job of transforming error for user consumption
+      // this.log(`${operation} failed: ${error.message}`);
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
 }
